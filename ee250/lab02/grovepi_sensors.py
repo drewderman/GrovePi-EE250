b49d02798b23a03b5886d1787e0b910006a754f6
drewderman <druderma@usc.edu>
2019-09-18 00:18:40 -0700

    9.18, should work

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -24,102 +24,31 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+from grove_rgb_lcd import * # imports all LCD stuff
 
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
-    PORT = 4    # D4
+
+    rangefinder_port = 4    # D4 - Rangefinder in digital port
+    potentiometer_port = 0   # A0 - Potentiometer in analog port
+    # LCD connected to I2C, no need to set this
+    setRBG(0, 255, 0) # set LCD to blue initially
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
+        
+        threshold = grovepi.analogRead(potentiometer_port) # read the potentiometer's value and set it to threshold
+        distance = grovepi.analogRead(rangefinder_port) # read the rangefinder's value and set it to distance
 
-        print(grovepi.ultrasonicRead(PORT))
+        if (distance < threshold): # only display error if rangefinder is less than the threshold
+            obj_error = "OBJ PRES" # output error that object is in the way
+            set RBG(255, 0, 0) # set LCD to red to say there is an error
+        else:
+            obj_error = "        " # clear obj pres if no object in the way
+            set RBG(0, 0, 255) # set LCD to green for a good reading
 
-# ----- setup LCD ------
-if sys.platform == 'uwp':
-    import winrt_smbus as smbus
-    bus = smbus.SMBus(1)
-else:
-    import smbus
-    import RPi.GPIO as GPIO
-    rev = GPIO.RPI_REVISION
-    if rev == 2 or rev == 3:
-        bus = smbus.SMBus(1)
-    else:
-        bus = smbus.SMBus(0)
-
-# this device has two I2C addresses
-DISPLAY_RGB_ADDR = 0x62
-DISPLAY_TEXT_ADDR = 0x3e
-
-# set backlight to (R,G,B) (values from 0..255 for each)
-def setRGB(r,g,b):
-    bus.write_byte_data(DISPLAY_RGB_ADDR,0,0)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,1,0)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,0x08,0xaa)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,4,r)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,3,g)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,2,b)
-
-# ----------- POTENTIOMETER ---------
-
-# Connect the Grove Rotary Angle Sensor to analog port A0
-# SIG,NC,VCC,GND
-potentiometer = 0
-
-# Reference voltage of ADC is 5v
-adc_ref = 5
-
-# Vcc of the grove interface is normally 5v
-grove_vcc = 5
-
-# Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
-full_angle = 300
-
-while True:
-    try:
-        # Read sensor value from potentiometer
-        potentiom_value = grovepi.analogRead(potentiometer)
-
-        # Calculate voltage
-        voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
-
-        # Calculate rotation in degrees (0 to 300)
-        degrees = round((voltage * full_angle) / grove_vcc, 2)
-
-        grovepi.ledBar_init(ledbar, 0)
-
-        # clear screen
-        textCommand(0x01)
-
-        # sleep .05
-        time.sleep(.05)
-
-        setText("test")
-
-    except IOError:
-        print ("Error")
-
-# sets text via setText("sampletext")
-def setText(text):
-    textCommand(0x01) # clear display
-    time.sleep(.05)
-    textCommand(0x08 | 0x04) # display on, no cursor
-    textCommand(0x28) # 2 lines
-    time.sleep(.05)
-    count = 0
-    row = 0
-    for c in text:
-        if c == '\n' or count == 16:
-            count = 0
-            row += 1
-            if row == 2:
-                break
-            textCommand(0xc0)
-            if c == '\n':
-                continue
-        count += 1
-        bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
+        setText_norefresh(str(threshold) + "cm " + obj_error + "\n" + str(distance) + "cm") # output threshold and error message (if needed) on first line and distance on second line
